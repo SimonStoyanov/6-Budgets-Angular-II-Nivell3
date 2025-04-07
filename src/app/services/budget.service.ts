ace43618dfa29dc6b74c71aec5866099e0135361
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2025-04-07 12:52:04 +0200

    Updated the BudgetService to include a Budget[] that will be sent to create dynamic panels

--- src/app/services/budget.service.ts
+++ src/app/services/budget.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
 
+interface Budget {
+  title: string,
+  description: string,
+  price: number,
+  select: boolean
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class BudgetService {
+
+  private budgets: Budget[] = [
+    {
+      title: "Seo",
+      description: "Programació d'una web responsive completa",
+      price: 300,
+      select: false
+    },
+    {
+      title: "Ads",
+      description: "Programació d'una web responsive completa",
+      price: 400,
+      select: false 
+    },
+    {
+      title: "Web",
+      description: "Programació d'una web responsive completa",
+      price: 500,
+      select: false
+    }
+  ];
+
+  getBudgets(): Budget[] {
+    return this.budgets;
+  }
 
   constructor() { }
 }
