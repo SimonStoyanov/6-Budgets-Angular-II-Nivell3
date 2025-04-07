6d83cc66ee24dd0daa346918f79ed1c9353f5d6f
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2025-04-07 12:50:37 +0200

    Created the Welcome component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { WelcomeComponent } from '../welcome/welcome.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [WelcomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
